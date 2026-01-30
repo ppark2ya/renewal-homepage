2809d59acba754b7959cadc3e9af3024eb5e2927
ppark2ya <vigabhan1231@gmail.com>
2026-01-30 09:51:27 +0900

    다국어 사용 환경 추가

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,41 @@
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Dozn Exchange | Best Rate, Faster Exchange",
+  description:
+    "365 days, 24 hours, the closest unmanned exchange kiosk. Exchange foreign currency quickly and safely anywhere in the country at Dozn Exchange.",
+};
+
+export default async function RootLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+}) {
+  const {locale} = await params;
+  
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+ 
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+ 
+  return (
+    <html lang={locale}>
+      <body className="antialiased">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
